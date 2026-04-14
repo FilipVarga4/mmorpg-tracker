b63227f4b215ff5969238526bdf3ad9dd8ff068b
FilipVarga4 <filip.varga4@student.ukf.sk>
2026-04-14 22:12:17 +0200

    feat: create header and footer templates

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+    <p>&copy; 2026 MMORPG Tracker</p>
+</footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MMORPG BiS Tracker</title>
+</head>
+<body>
+<header>
+    <h1>Tracker Progresie & Gear Rating</h1>
+    <nav>
+        <a href="index.php">Domov</a> |
+        <a href="add_character.php">Pridať postavu</a>
+    </nav>
+</header>
+<main>
